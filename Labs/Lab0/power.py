ceda61b91c2fee0875a7a0beebc0cbce1afbf342
Huilin-Feng <feng.huil@northeastern.edu>
2026-01-09 14:12:56 -0800

    Complete power function

--- Labs/Lab0/power.py
+++ Labs/Lab0/power.py
@@ -1,1 +1,13 @@
 # Lab 0 Part II D
+def power(base, exponent):
+    """
+    Computes base raised to the power of exponent.
+
+    :param base: int or float
+    :param exponent: non-negative integer
+    :return: base ** exponent
+    """
+    result = 1
+    for _ in range(exponent):
+        result *= base
+    return result
