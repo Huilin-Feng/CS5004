bb9144776c2041027837f626582dfb78857d9eeb
Huilin-Feng <feng.huil@northeastern.edu>
2026-01-09 14:18:56 -0800

    Create AddFromKbd.java

---
+++ Labs/Lab0/AddFromKbd.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+// Lab 0 Part II G
+public class AddFromKbd {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int a = input.nextInt();
+        int b = input.nextInt();
+
+        System.out.println(a + b);
+    }
+}
