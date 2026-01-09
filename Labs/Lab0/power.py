23b1ccf4f1f26411455ec0ef1517a6d91e13553f
Huilin-Feng <feng.huil@northeastern.edu>
2026-01-09 14:15:25 -0800

    Create power.py

--- Labs/Lab0/power.py
+++ Labs/Lab0/power.py
@@ -1,13 +1,3 @@
 # Lab 0 Part II D
-def power(base, exponent):
-    """
-    Computes base raised to the power of exponent.
-
-    :param base: int or float
-    :param exponent: non-negative integer
-    :return: base ** exponent
-    """
-    result = 1
-    for _ in range(exponent):
-        result *= base
-    return result
+result = 2 ** 31
+print(result)
